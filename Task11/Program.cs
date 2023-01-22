1ab6a389d07aeac2d1ffa5f390e6573ce3be6c70
Makc <m_stefanenkov@mail.ru>
2023-01-22 14:03:34 +0300

    Решили задачу 11

---
+++ Task11/Program.cs
@@ -0,0 +1,26 @@
+﻿/*12. Напишите программу, которая будет принимать на
+вход два числа и выводить, является ли первое число
+кратным второму. Если число 1 не кратно числу 2, то
+программа выводит остаток от деления.
+34, 5 -> не кратно, остаток 4
+16, 4 -> кратно
+*/
+int MultipleNums(int num1, int num2)
+{
+    return num1 % num2;
+}
+Console.Write("введите целое число1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите целое число2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int remains = MultipleNums(number1, number2);
+
+if (remains == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Остаток от деления составляет {remains}");
+}
